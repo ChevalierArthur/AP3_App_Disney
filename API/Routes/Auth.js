59fa4e4758afa24bd4e0ca6587a44049ff462b38
ChevalierArthur <arthur.chevalier@campuscarloacutis.fr>
2026-03-24 15:00:16 +0100

    <feature> route ajout utilisateur et login créer

---
+++ API/Routes/Auth.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
+function creerToken(userId) {
+    return jwt.sign(
+        { id: userId },
+        process.env.JWT_SECRET,
+        { expiresIn: '10m' }
+    );
+}
+
+function auth(req, res, next) {
+    const header = req.headers.authorization;
+
+    if (!header) {
+        return res.status(401).json({ message: 'token manquant' });
+    }
+
+    const token = header.split(' ')[1];
+
+    try {
+        req.user = jwt.verify(token, process.env.JWT_SECRET);
+        next();
+    } catch {
+        return res.status(403).json({ message: 'token invalide' });
+    }
+}
+router.get('/', (req, res) => {
+    res.send("login GET works")
+});
+
+router.post('/login', (req, res) => {
+    const { login, mdp } = req.body;
+    const query = 'SELECT idUtilisateur, mdpUtilisateur, idEquipeUtilisateur FROM users WHERE identifiantUtilisateur = ? ';
+    config.query(query, [login], (err, results) => {
+        if (err) return res.status(500).json({ message: 'erreur bdd' });
+        if (results.length === 0) {
+            return res.status(401).json({ message: 'login incorrect' });
+        }
+        const user = results[0];
+        if (user.mdpUtilisateur !== mdp) {
+            return res.status(401).json({ message: 'mdp incorrect' });
+        }
+        const token = creerToken(user.idUtilisateur);
+        res.json({ id:user.idUtilisateur, token, role: user.idEquipeUtilisateur });
+    }
+    );
+});
+
+router.get('/create_user', auth, (req, res) => {
+    const { login, mdp, role } = req.body;
+    const query = 'INSERT INTO users (nomUtilisateur, prenomUtilisateur, identifiantUtilisateur, mdpUtilisateur, idEquipeUtilisateur) VALUES (?, ?, ?, ?, ?)';
+    config.query(query, [login, mdp, role], (err, results) => {
+        if (err) return res.status(500).json({ message: 'erreur bdd' });
+        res.json({ message: 'utilisateur créé', id: results.insertId });
+    });
+});
+
+module.exports = router;
---
+++ API/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+const config = require('./bdd.js');
+app.use(express.json());
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+
+
+app.use('/auth', require('./Routes/Auth.js'));
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
